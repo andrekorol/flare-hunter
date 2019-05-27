226f1fed9ee12c7a0d24223fc3c7c3d27e485b79
andrekorol <anrobits@yahoo.com.br>
2019-05-27 14:32:38 -0300

    Add new plots

--- journal/plot.py
+++ journal/plot.py
@@ -11,48 +11,6 @@ for fits_list in json_data:
 
     # Plot and save the figure
     print(title)
-    if title == "HUMAIN":
-        hours_xticks = ['14:00',  '14:00', '14:12', '14:24', '14:36', '14:48',
-                        '15:00', '15:12', '15:24', '15:36', '15:36']
-        fitsread.ECallistoFitsFile.plot_fits_files_list(fits_list, title,
-                                                        title, 'en', 100, 350,
-                                                        hours_xticks)
-
-    elif title == "INPE":
-        hours_xticks = ['14:00', '14:11', '14:23', '14:35', '14:46', '14:58',
-                        '15:10', '15:21', '15:33']
-        fitsread.ECallistoFitsFile.plot_fits_files_list(fits_list, title,
-                                                        title, 'en', 100, 350,
-                                                        hours_xticks)
-
-    elif title == "TRIEST":
-        hours_xticks = ['14:00', '14:11', '14:23', '14:35', '14:46', '14:58',
-                        '15:10', '15:21', '15:33']
-        fitsread.ECallistoFitsFile.plot_fits_files_list(fits_list, title,
-                                                        title, 'en', None, 390,
-                                                        hours_xticks)
-
-    elif title == 'SWMC' or title == 'UNAM' or title == 'BIR':
-        hours_xticks = ['14:00', '14:11', '14:23', '14:35', '14:46', '14:58',
-                        '15:10', '15:21', '15:33']
-        fitsread.ECallistoFitsFile.plot_fits_files_list(fits_list, title,
-                                                        title, 'en', None,
-                                                        None, hours_xticks)
-    elif title == "BLEN7M":
-        hours_xticks = ['14:00', '14:11', '14:23', '14:35', '14:46', '14:58',
-                        '15:10', '15:21', '15:33']
-        fitsread.ECallistoFitsFile.plot_fits_files_list(fits_list, title,
-                                                        title, 'en', 100, 420,
-                                                        hours_xticks)
-
-    elif title == "OSRA":
-        hours_xticks = ['14:00', '14:11', '14:23', '14:35', '14:46', '14:58',
-                        '15:10', '15:21', '15:33']
-        fitsread.ECallistoFitsFile.plot_fits_files_list(fits_list, title,
-                                                        title, 'en', None, 420,
-                                                        hours_xticks)
-
-    else:
-        fitsread.ECallistoFitsFile.plot_fits_files_list(fits_list, title,
-                                                        title, 'en', None,
-                                                        None)
+    fitsread.ECallistoFitsFile.plot_fits_files_list(fits_list, title,
+                                                    title, 'en', None,
+                                                    None)
