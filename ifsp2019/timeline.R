48c55b6979cbc3eabd4508caf2e65cb576dedec5
andrekorol <anrobits@yahoo.com.br>
2019-09-14 00:06:22 -0300

    Add final versions of the timeline

--- ifsp2019/timeline.R
+++ ifsp2019/timeline.R
@@ -3,3 +3,5 @@ library(vistime)
 df <- read.csv("events.csv")
 
 timeline <- vistime(df, title = "Linha do tempo de eventos detectados na zona ativa (AR) 1158")
+
+orca(timeline, file = "timeline.png", width = 1200, height = 420, scale = 8)
